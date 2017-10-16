c6c4a436d7b030e94899260f157f659a3dc07207
Khuthadzo TSHIKOTSHI <ktshikot@e4r1p2.wethinkcode.co.za>
2017-10-16 15:56:17 +0200

    add to source control

---
+++ ComputerV1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using  System.Text.RegularExpressions;
+
+namespace ComputerV1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] expr;
+            
+
+            if (args.Length != 1)
+            {
+                Console.WriteLine("No expression provided");
+            }
+            else
+            {
+                expr = Regex.Split(args[0].Replace(" ", ""), @"(\-)|(\+)|(\=)");
+                simplify(expr);
+                if (getDegree(expr))
+                {
+                    Console.WriteLine("Yay");
+                }
+            }
+
+        }
+
+        public static bool getDegree(string[] expression)
+        {
+            int tmp = 0;
+            int strt = 0;
+            int degree = 0;
+            
+            foreach (var VARIABLE in expression)
+            {
+                if (VARIABLE.Contains("^"))
+                {
+                    strt = VARIABLE.IndexOf('^') + 1;
+                    if (Int32.TryParse(VARIABLE.Substring(strt), out tmp))
+                    {
+                        if (tmp > degree)
+                            degree = tmp;
+                    }
+                }
+            }
+            Console.WriteLine("Polynomial degree: {0}", degree);
+            if (degree > 2)
+            {
+                Console.WriteLine("The polynomial degree is stricly greater than 2, I can't solve.");
+                return (false);
+            }
+            return (true);
+        }
+
+        public static string[] simplify(string[] expr)
+        {
+            Int16 indx = 0;
+            foreach (var VARIABLE in expr)
+            {
+                if (VARIABLE != "=")
+                    indx++;
+                else
+                    break;
+            }
+            Console.WriteLine(indx);
+            return (expr);
+        }
+    }
+}
